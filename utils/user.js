4d78873325516a41a9d71479a1b555349cbf01c2
Nada <nadafarrag619@gmail.com>
2024-09-01 23:37:03 +0300

    file

---
+++ utils/user.js
@@ -0,0 +1,30 @@
+import redisClient from './redis';
+import dbClient from './db';
+/**
+ * User utilities
+ */
+const userUtils = {
+  /**
+   * Gets a user id and key of redis from request
+   * @request {request_object} express request obj
+   * @return {object} object containing userId
+   */
+  async getUserIdAndKey(request) {
+    const obj = { userId: null, key: null };
+    const xToken = request.header('X-Token');
+    if (!xToken) return obj;
+    obj.key = `auth_${xToken}`;
+    obj.userId = await redisClient.get(obj.key);
+    return obj;
+  },
+  /**
+   * Gets a user from database
+   * @query {object} query expression
+   * @return {object} user document object
+   */
+  async getUser(query) {
+    const user = await dbClient.usersCollection.findOne(query);
+    return user;
+  },
+};
+export default userUtils;
